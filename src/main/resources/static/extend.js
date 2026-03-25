efe05bace058ed2c3895c1716692b41f8d201633
정준식 <159753martin@naver.com>
2026-03-25 13:47:49 +0900

    aa

--- src/main/resources/static/extend.js
+++ src/main/resources/static/extend.js
@@ -29,7 +29,8 @@
 		{ name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
 		{ name: 'mobile-web-app-capable', content: 'yes' },
 		{ name: 'apple-mobile-web-app-capable', content: 'yes' },
-		{ name: 'apple-mobile-web-app-status-bar-style', content: 'default' },
+/*		{ name: 'apple-mobile-web-app-status-bar-style', content: 'default' },*/
+		{ name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' }
 	];
 	metas.forEach(({ name, content }) => {
 		const meta = document.createElement('meta');
